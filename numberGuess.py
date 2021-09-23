4c38e542ce0efe852c1a3b62ba16a33420a59d05
Srisanth4944s <70645902+Srisanth4944s@users.noreply.github.com>
2021-09-23 18:10:44 +0530

    Add files via upload

---
+++ numberGuess.py
@@ -0,0 +1,16 @@
+#Integer variable
+number=7
+
+number = int(input("guess a number till 1-10 :"))
+print(number)
+
+if (number<8):
+    print("You are good at guss")
+elif (number<2):
+    print("not bad try again")
+#else:
+ #   print("I know what you feel!")
+
+
+
+
